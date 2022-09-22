c4f73a8251507ca8d4864fcffe688cbe7aafe672
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-09-22 16:07:15 +0300

    hw-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import VimeoPlayer from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new VimeoPlayer(iframe);
+const onPlay = function (data) {
+  console.log('played the video!');
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+  console.log(localStorage);
+};
+const throttleOnPlay = throttle(onPlay, 1000);
+player.on('timeupdate', throttleOnPlay);
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    localStorage.getItem('videoplayer-current-time');
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
