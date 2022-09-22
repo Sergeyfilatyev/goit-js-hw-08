33da284ad35442eed535d29709e97dd8db1cb9f3
Sergey Filatyev <filatevsergey2407@gmail.com>
2022-09-22 13:06:30 +0300

    hw1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryBox = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(({ description, original, preview }) => {
+    return `
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}" 
+      />
+  </a>`;
+  })
+  .join('');
+galleryBox.insertAdjacentHTML('afterbegin', markup);
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
